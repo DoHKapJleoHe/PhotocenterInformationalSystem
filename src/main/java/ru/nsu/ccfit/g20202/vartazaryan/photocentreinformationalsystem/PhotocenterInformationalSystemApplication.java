18263daf3c22a869335c03a7d293a1c7168739e5
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-04-11 19:13:54 +0700

    11.04.2022: Started project

---
+++ src/main/java/ru/nsu/ccfit/g20202/vartazaryan/photocentreinformationalsystem/PhotocenterInformationalSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.g20202.vartazaryan.photocentreinformationalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotocenterInformationalSystemApplication
+{
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(PhotocenterInformationalSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/nsu/ccfit/g20202/vartazaryan/photocentreinformationalsystem/PhotocentreInformationalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.g20202.vartazaryan.photocentreinformationalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotocentreInformationalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
